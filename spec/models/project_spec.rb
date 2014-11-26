a78cbdc0a77f1165d92153c11cfd5075e15aad86
koshikawa <koshikawa@ppworks.jp>
2014-11-27 06:33:58 +0900

    More simple

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,14 +5,4 @@ RSpec.describe Project do
     subject { create(:project) }
     it { expect(subject).to be_persisted }
   end
-
-  describe 'create projects' do
-    let(:project_count) { 10 }
-    subject { create_list(:project, project_count) }
-    it do
-      expect {
-        subject
-      }.not_to raise_error
-    end
-  end
 end
