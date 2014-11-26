b3fb2b394c6624f79499c9e4a44a08110752466b
koshikawa <koshikawa@ppworks.jp>
2014-11-27 00:34:13 +0900

    Setting for capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,14 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.after(:each) do |example|
+    DatabaseCleaner.clean
+  end
 end
