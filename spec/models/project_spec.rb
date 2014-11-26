730fd512734cb77017fc0dd52098ac5015fd6f84
koshikawa <koshikawa@ppworks.jp>
2014-11-27 01:20:26 +0900

    Db unique constraint

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Project do
     subject { create(:project) }
     it { expect(subject).to be_persisted }
   end
+
+  describe 'create projects' do
+    let(:project_count) { 10 }
+    subject { create_list(:project, project_count) }
+    it do
+      expect {
+        subject
+      }.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+  end
 end
