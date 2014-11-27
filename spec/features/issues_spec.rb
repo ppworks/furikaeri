e118a0eb7642ae1584d9a3f92834e087addfa87e
koshikawa <koshikawa.naoto@moneyforward.co.jp>
2014-11-27 09:59:59 +0900

    DRY

--- spec/features/issues_spec.rb
+++ spec/features/issues_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-describe 'create new issue' do
-  let(:project) { create(:project) }
-  let(:new_issue) { build(:issue) }
+shared_examples_for 'create new issue' do
   feature 'create new issue' do
     scenario do
       visit project_path(project)
@@ -11,11 +9,32 @@ describe 'create new issue' do
         select(new_issue.status_text, from: 'issue_status')
         find('input[type=submit]').click
       end
+
       expect(page).to have_content('created new issue')
-      within("ul.#{new_issue.status}") do
+      within("ul.#{status}") do
         expect(page).to have_content(new_issue.title)
       end
       expect(current_path).to eq project_path(project)
     end
+  end
+end
+
+describe 'Issues' do
+  let(:project) { create(:project) }
+  let(:new_issue) { build(:issue, status) }
+
+  context 'keep' do
+    let(:status) { :keep }
+    it_behaves_like 'create new issue'
+  end
+
+  context 'problem' do
+    let(:status) { :problem }
+    it_behaves_like 'create new issue'
+  end
+
+  context 'try' do
+    let(:status) { :try }
+    it_behaves_like 'create new issue'
   end
 end
