a509c4ef980d518aaf583d01dfba2aee65e067f2
koshikawa <koshikawa@ppworks.jp>
2014-11-30 03:13:09 +0900

    rails g migration AddPriorityToIssues priority:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126215936) do
+ActiveRecord::Schema.define(version: 20141129181229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141126215936) do
     t.string   "status",     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "priority"
   end
 
   add_index "issues", ["project_id"], name: "index_issues_on_project_id", using: :btree
